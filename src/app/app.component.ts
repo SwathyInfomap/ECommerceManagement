da6f0fdee0af885b0e74394501ca00e89cd94c23
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-25 16:23:06 +0530

    Home Component added with routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet ,HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet ,HeaderComponent,FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
