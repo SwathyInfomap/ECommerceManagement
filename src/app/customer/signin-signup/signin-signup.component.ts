5d34ffd0cf888fa6baceb32efb0ea923b03b9c9c
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-26 15:19:04 +0530

    signIn signUp page

--- src/app/customer/signin-signup/signin-signup.component.ts
+++ src/app/customer/signin-signup/signin-signup.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './signin-signup.component.html',
   styleUrl: './signin-signup.component.css'
 })
