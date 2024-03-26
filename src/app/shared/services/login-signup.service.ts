9ab12bd5813031021d50fca13dc2afe9e5e52573
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-26 11:36:38 +0530

    added api service and login services

--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginSignupService {
 
-  constructor() { }
+  /**PUBLIC URLS NEED TO BE DEFINED HERE */
+  public login_url = "http://localhost:3000";
+  public reg_url = "http://localhost:3000";
+
+  /**API Service for constructor we get from app->core -->service */
+
+  constructor(private http:HttpClient, private apiService:ApiService) { }
+  authLogin(user_name:any, password:any):Observable<any>{
+     return this.apiService.get(this.login_url+'/user?email='+user_name+'&password='+password);
+  }
+
+  userRegister(user_dto:any):Observable<any>{
+    return this.apiService.post(this.reg_url+'/user',user_dto)
+  }
+
+  adminLogin(user_name:any, password:any):Observable<any>{
+    return this.apiService.get(this.login_url+'/user?email='+user_name+'&password='+password+'&role=admin');
+ }
+
 }
